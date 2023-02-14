e961ae8436d2ce60e11fef3d1504197cb1c3840d
Sourcegraph <batch-changes@sourcegraph.com>
2023-02-14 23:45:50 +0000

    Replace the standard library "log" with logrus

--- fws/resource_fws_database.go
+++ fws/resource_fws_database.go
@@ -2,7 +2,7 @@ package fws
 
 import (
 	"fmt"
-	"log"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-fakewebservices/client"
